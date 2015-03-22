110f291260b64a9df3ebf31d5f79cc77b5f788df
bguiz <brendan.graetz@gmail.com>
2015-03-23 09:49:01 +1100

    =BG= implement remainign tests for add one plugin
    - using the helpers for creating and removing demo plugins on the fly

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,10 @@
 'use strict';
 
+var path = require('path');
+
 var pluginRegistry = require('../index');
+
+var helper = require('./helper');
 
 describe('[basic]', function() {
   describe('[access]', function() {
@@ -17,10 +21,11 @@ describe('[basic]', function() {
         expect(typeof instance.add).toBe('function');
         expect(typeof instance.getAllOfCategory).toBe('function');
         expect(typeof instance.getFullRegistry).toBe('function');
+        expect(typeof instance.getOptions).toBe('function');
         done();
       });
 
-      it('Should be able to create mutliple registries', function(done) {
+      it('Should be able to create multiple registries', function(done) {
         var instance1;
         var instance2;
         expect(function() {
@@ -48,7 +53,7 @@ describe('[basic]', function() {
         done();
       });
 
-      it('Should not be able to create mutliple default registries', function(done) {
+      it('Should not be able to create multiple default registries', function(done) {
         var instance1;
         var instance2;
         expect(function() {
@@ -70,19 +75,39 @@ describe('[basic]', function() {
         }).not.toThrow();
         expect(function() {
           instance2.options({ myOption: 456 });
-        }).toThrow('Can only set options once for registry foo');
+        }).toThrowError('Can only set options once for registry foo');
         done();
       });
     });
 
     describe('[add one plugin]', function() {
+      //TODO replace with `beforeAll` once we get jasmine 2.1
+      beforeEach(function(done) {
+        helper.createPlugin({
+          requirePath: './node_modules/foo-plugin',
+          name: 'foo-plugin',
+          category: 'generic-plugin',
+        }, function onPluginCreated() {
+          done();
+        });
+      });
+
+      //TODO replace with `afterAll` once we get jasmine 2.1
+      afterEach(function(done) {
+        helper.cleanUpPlugin({
+          requirePath: './node_modules/foo-plugin',
+        }, function onPluginRemoved() {
+          done();
+        });
+      });
+
       it('Should validate plugin definitions', function(done) {
         var instance1;
         var instance2;
 
         expect(function() {
           instance1 = pluginRegistry.get(42);
-        }).toThrow('Invalid name for registry');
+        }).toThrowError('Invalid name for registry');
 
         instance2 = pluginRegistry.get('foo');
 
@@ -90,13 +115,13 @@ describe('[basic]', function() {
           instance2.add({
             name: 'task-name',
           });
-        }).toThrow('Plugins should have a category');
+        }).toThrowError('Plugins should have a category');
 
         expect(function() {
           instance2.add({
             category: 'task',
           });
-        }).toThrow('Plugins should have a name');
+        }).toThrowError('Plugins should have a name');
 
         expect(function() {
           instance2.add({
@@ -104,34 +129,75 @@ describe('[basic]', function() {
             category: 'task',
             requirePath: './relative/path',
           });
-        }).toThrow('Require path should resolve to an absolute path');
+        }).toThrowError('Require path should resolve to an absolute path');
 
         done();
       });
 
-    //TODO @bguiz - remaining tests
-      it('Should register a single plugin');
-      it('Should register a single plugin with name string only', function(done) {
-        //TODO create a mock plugin folder to test this
-        // expect(function() {
-        //   instance2.add('a-string-task');
-        // }).not.toThrow();
+      //TODO @bguiz - remaining tests
+      it('Should register a single plugin', function(done) {
+        var instance2;
+        instance2 = pluginRegistry.get('foo');
+        expect(function() {
+          instance2.add({
+            name: 'foo-plugin',
+            category: 'generic-plugin',
+          });
+        }).not.toThrow();
         done();
       });
-      it('Should test for files in the various possible locations');
-      it('Should load plugin with a explicit require path');
-      it('Should not load a plugin with an explicit require path where the path is not absolute');
+
+      it('Should register a single plugin with string (name) only', function(done) {
+        var instance2;
+        instance2 = pluginRegistry.get('foo');
+        expect(function() {
+          instance2.add('foo-plugin');
+        }).not.toThrow();
+        done();
+      });
+
+      it('Should load plugin with a explicit require path', function(done) {
+        var instance2;
+        instance2 = pluginRegistry.get('foo');
+        expect(function() {
+          instance2.add({
+            requirePath: path.resolve(__dirname, '../node_modules/foo-plugin'),
+            name: 'foo-plugin',
+            category: 'generic-plugin',
+          });
+        }).not.toThrow();
+        done();
+      });
+
+      it('Should not load a plugin with an explicit require path where the path is not absolute', function(done) {
+        var instance2;
+        instance2 = pluginRegistry.get('foo');
+        expect(function() {
+          instance2.add({
+            requirePath: '../node_modules/foo-plugin',
+            name: 'foo-plugin',
+            category: 'generic-plugin',
+          });
+        }).toThrowError('Require path should resolve to an absolute path');
+        done();
+      });
     });
 
     describe('[add multiple plugins]', function() {
       it('Should register multiple plugins specified as an array');
+
       it('Should register multiple plugins specified as arguments');
+
+      it('Should test for files in the various possible locations');
     });
 
     describe('[query registry]', function() {
       it('Should clear registry when reset called');
+
       it('Should get all of one category');
-      it('Should get empty array when get all of category called on category thta does not yet exist');
+
+      it('Should get empty array when get all of category called on category that does not yet exist');
+
       it('Should get entire registry');
     });
   });
