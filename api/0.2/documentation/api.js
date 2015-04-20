3503961b83b57d53751e918f92c924b3b10a3e4d
autodocs Git User <autodocs-git-user@bguiz.com>
2015-04-20 07:30:46 +0000

    autodocs publish 2015-04-20:07:30:46 595c51b

---
+++ api/0.2/documentation/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "PluginRegistry",
+        "PluginRegistryFluentInterface"
+    ],
+    "modules": [
+        "PluginRegistry"
+    ],
+    "allModules": [
+        {
+            "displayName": "PluginRegistry",
+            "name": "PluginRegistry"
+        }
+    ]
+} };
+});
